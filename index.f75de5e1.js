1351cca86a519976680b2136edb66f359789e60b
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-07 16:50:10 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.querySelector("body");
+    const block = document.createElement("div");
+    block.className = `notification ${type}`;
+    block.style.display = "relative";
+    block.style.top = `${posTop}px`;
+    block.style.right = `${posRight}px`;
+    block.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+    body.append(block);
+    setTimeout(()=>{
+        block.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
